5ee8939bc174619bd1bf8a4bdfc948d7f1b6b312
Quico Moya <quico@moya.ws>
2013-03-10 21:26:50 +0100

    Initial Commit

---
+++ EmptyTests/EmptyTests.h
@@ -0,0 +1,13 @@
+//
+//  EmptyTests.h
+//  EmptyTests
+//
+//  Created by Quico Moya on 3/10/13.
+//  Copyright (c) 2013 Quico Moya. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EmptyTests : SenTestCase
+
+@end
---
+++ EmptyTests/EmptyTests.m
@@ -0,0 +1,32 @@
+//
+//  EmptyTests.m
+//  EmptyTests
+//
+//  Created by Quico Moya on 3/10/13.
+//  Copyright (c) 2013 Quico Moya. All rights reserved.
+//
+
+#import "EmptyTests.h"
+
+@implementation EmptyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EmptyTests");
+}
+
+@end
